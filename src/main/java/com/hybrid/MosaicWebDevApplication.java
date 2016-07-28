1c5b16bd2f890efc7de5eb56d024fd69e66ce54f
java <java>
2016-07-28 12:37:57 +0900

    first commit

---
+++ src/main/java/com/hybrid/MosaicWebDevApplication.java
@@ -0,0 +1,12 @@
+package com.hybrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosaicWebDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosaicWebDevApplication.class, args);
+	}
+}
